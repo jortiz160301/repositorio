9b018ebfa9499bd6fa2a1c094fd0002ad21e0b3c
jortiz160301 <josecarloscristorey16@gmail.com>
2019-11-08 13:15:09 +0100

    cambio de prueba

--- programas/ej1_7.cpp
+++ programas/ej1_7.cpp
@@ -1,8 +1,8 @@
-#include <iostream>
+﻿#include <iostream>
 using namespace std;
 
 int main(){
-double num1, num2, suma, resta, mult, div;
+double num1, num2, suma, resta, mult, div;//declaración de variables
 cout<<"¡¡¡Bienvenid@ a la mini calculadora CalcuTron!!! Por favor, introduzca dos números reales y se le calculará la suma, resta, multiplicación y división. (Recuerde solo escribir números o el resultado no tendrá sentido)."<<endl;
 cin>> num1 >> num2;
 suma = num1 + num2; //sumo
